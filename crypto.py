b2e9b2a28e56089598ef9a14edba647ea7b5ad71
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2017-09-25 18:45:23 +0800

    add crypto functions experiments

---
+++ crypto.py
@@ -0,0 +1,11 @@
+import hashlib
+
+print hashlib.sha224("Albert Chen").hexdigest()
+print hashlib.sha224("albert chen").hexdigest()
+print hashlib.sha224("Allen Ha MH").hexdigest()
+print hashlib.sha224("Amedeo Spagnolo").hexdigest()
+print hashlib.sha224("Dickson Ma").hexdigest()
+print hashlib.sha224("Jeff Chiu").hexdigest()
+print hashlib.sha224("Jessie Chan").hexdigest()
+print hashlib.sha224("Raymond Hui").hexdigest()
+print hashlib.algorithms
